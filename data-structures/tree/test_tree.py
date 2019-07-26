6a1346200f36be4a4a80148b92e2bc0f66280a20
arsummers <aliyasummers1@gmail.com>
2019-07-26 14:36:05 -0700

    tree test fixture written

--- data-structures/tree/test_tree.py
+++ data-structures/tree/test_tree.py
@@ -1,3 +1,32 @@
 import pytest
 from tree import BinaryTree, Node
 
+def test_exists():
+    assert BinaryTree
+    assert Node
+
+@pytest.fixture
+def tree():
+    one =  Node(1)
+    two = Node(2)
+    three = Node(3)
+    four = Node(4)
+    five = Node(5)
+    six = Node(6)
+    seven = Node(7)
+    eight = Node(8)
+    nine = Node(9)
+  
+    one.left = two
+    one.right = three
+    three.left = four
+    three.right = five
+    two.left = six
+    six.right = seven
+    seven.left = eight
+    seven.right = nine
+
+    arbor = BinaryTree
+    arbor.root = one
+
+    return arbor
