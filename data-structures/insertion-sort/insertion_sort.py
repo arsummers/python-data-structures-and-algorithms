491c380b969518541a13fb3d8c2bbebaca6e6471
arsummers <aliyasummers1@gmail.com>
2019-08-12 16:18:23 -0700

    tests passing

--- data-structures/insertion-sort/insertion_sort.py
+++ data-structures/insertion-sort/insertion_sort.py
@@ -1,2 +1,12 @@
 def insertion_sort(lst):
-    pass
+    for i in range(len(lst)):
+        j = (i - 1)
+        temp = lst[i]
+
+        while j >= 0 and temp < lst[j]:
+            lst[j+1] = lst[j]
+            j = j-1
+
+        lst[j + 1] = temp
+    return lst
+    
