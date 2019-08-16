763c7f398a094a3ee5f9908299aee109c4c5bef9
arsummers <aliyasummers1@gmail.com>
2019-08-16 13:17:54 -0700

    tests written

---
+++ data-structures/hashtable/hashtable.py
@@ -0,0 +1,17 @@
+# from linked_list.linked_list import LinkedList
+
+class HashTable:
+    def __init__(self):
+        buckets = self.buckets
+
+    def hash(self, key):
+        pass
+
+    def add(self, key, value):
+        pass
+
+    def get(self, key):
+        pass
+
+    def contains(self, key):
+        pass
---
+++ data-structures/hashtable/test_hashtable.py
@@ -0,0 +1,51 @@
+import pytest
+from hashtable import HashTable
+
+def test_exists():
+    assert HashTable
+
+def test_get_missing():
+    ht = HashTable
+
+    with pytest.raises(ValueError):
+        ht.get('cats')
+
+def test_add():
+    ht = HashTable
+    ht.add('spam', 'eggs')
+
+    assert ht.get('spam') == 'eggs'
+
+def test_contains():
+    ht = HashTable
+    ht.add('spam', 'eggs')
+
+    assert ht.contains('spam')
+
+def test_not_contains():
+    ht = HashTable
+
+    assert not ht.contains('spam')
+
+def test_hash_same():
+    ht = HashTable
+
+    assert ht.hash('dogs') == ht.hash('gods')
+
+def test_hash_in_range():
+    ht = HashTable
+
+    assert 0 <= ht.hash('cats') > len(ht.buckets)
+
+def test_different_hashes():
+    ht = HashTable
+
+    assert not ht.hash('dogs') == ht.hash('cats')
+
+def test_collision():
+    ht = HashTable
+    ht.add('cats', 'cat things')
+    ht.add('acts', 'acting things')
+
+    assert ht.get('cats') == 'cat things'
+    assert ht.get('acts') == 'acting things'
