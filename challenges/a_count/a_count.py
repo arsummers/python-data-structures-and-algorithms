30d6a2f7012cb866dc582f03b3932d43c5522809
arsummers <aliyasummers1@gmail.com>
2020-04-22 11:25:55 -0700

    tests passing, unsure hackerrank will though

--- challenges/a_count/a_count.py
+++ challenges/a_count/a_count.py
@@ -1,4 +1,10 @@
 def a_count(s, n):
     a_num = 0
+    repeated_string = s*100
+    new_string = repeated_string[:n]
+
+    for letter in new_string:
+        if letter == 'a':
+            a_num += 1
 
     return a_num
--- challenges/a_count/test_a_count.py
+++ challenges/a_count/test_a_count.py
@@ -3,35 +3,35 @@ from a_count import a_count
 
 def test_short_cut():
     n = 3
-    s = 'babababa'
+    s = 'ba'
     expected = 1
     actual = a_count(s, n)
     assert expected == actual
 
 def test_aba():
     n = 10
-    s = 'abaabaabaabaabaabaabaabaabaaba'
+    s = 'aba'
     expected = 7
     actual = a_count(s, n)
     assert expected == actual
 
 def test_only_a():
     n = 5
-    s = 'aaaaaaaaaaaaaaaa'
+    s = 'a'
     expected = 5
     actual = a_count(s, n)
     assert expected == actual
 
 def test_no_a_in_string():
     n = 3
-    s = 'bcdbcdbcdbcdbcd'
+    s = 'bcd'
     expected = 0
     actual = a_count(s, n)
     assert expected == actual
 
 def test_no_a_in_slice():
     n = 4
-    s = ='bdcghabdcgha'
+    s = 'bdcgha'
     expected = 0
     actual = a_count(s, n)
     assert expected == actual
