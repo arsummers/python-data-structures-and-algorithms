ee9fa617594ecb2149fd5553a0dc1b4cdf10d62f
arsummers <aliyasummers1@gmail.com>
2020-04-21 17:53:21 -0700

    cleaned up, passes all my tests

--- challenges/cloud_jumping/cloud_jumping.py
+++ challenges/cloud_jumping/cloud_jumping.py
@@ -1,26 +1,24 @@
-c = [0, 0, 0, 0, 1, 0, 1, 0]
-# result should be 4
-
 def cloud_jumping(c):
     jump_count = 0
     position = 0
     length = len(c)
 
     for _ in range(length):
+        if length <= 3:
+            jump_count = 1
         if position < length and c[position] == 0:
-            print('hit first if')
-            print('postion:', position)
-            if position < length-2 and c[position+2] == 0:
-                print('hit second if')           
+
+            if position < length-2 and c[position+2] == 0:           
                 jump_count += 1
                 position += 2
-                print('postion:', position)
+
             elif position < length-2 and c[position+2] == 1:
-                print('hit third if')
                 jump_count += 1
                 position += 1
                 print('postion:', position)
 
-    print("jumps:", jump_count)
+            elif position < length-1 and c[position+1] == 0:
+                jump_count += 1
+                position += 1
 
-cloud_jumping(c)
+    return jump_count
