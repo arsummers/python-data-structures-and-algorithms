b55997a560ca3b85d959ea8d1cf61586d0cd05b9
arsummers <aliyasummers1@gmail.com>
2020-04-22 16:28:10 -0700

    passing all visible test cases, but not some invisible ones

--- challenges/2d_arr/two_d_arr.py
+++ challenges/2d_arr/two_d_arr.py
@@ -1,13 +1,10 @@
 def hourglass_sum(arr):
     hg_sum = 0
-    # hg_sum will shift a lot and get reassigned
+    for i in range(4):
+        for j in range(4):
+            temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
 
-    a = 0
-    b = 1
-    c = 2
-    d = 1
-    e = 0
-    f = 1
-    g = 2
+            if temp > hg_sum:
+                hg_sum = temp
 
     return hg_sum
