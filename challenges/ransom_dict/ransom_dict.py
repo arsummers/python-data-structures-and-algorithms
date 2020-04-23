955678be23e598e55b3e3927479604c1c75ed042
arsummers <aliyasummers1@gmail.com>
2020-04-22 20:35:00 -0700

    base tests written

---
+++ challenges/ransom_dict/ransom_dict.py
@@ -0,0 +1,8 @@
+# check if word is in dictionary
+# print Yes if note can be formed, No if not
+# case sensitive! which means I don't have to worry about it
+# going to have to put words into a dict, mark them as seen so they don't get accidentally re-used
+
+
+def check_magazine(magazine, note):
+    return 0
---
+++ challenges/ransom_dict/test_ransom_dict.py
@@ -0,0 +1,26 @@
+import pytest
+from ransom_dict import check_magazine
+
+def test_exists():
+    assert check_magazine
+
+def test_easy_sample():
+    magazine = ['give', 'me', 'one', 'grand', 'today', 'night']
+    note = ['give', 'one', 'grand', 'today']
+    expected = 'Yes'
+    actual = check_magazine(magazine, note)
+    assert expected == actual
+
+def test_insufficient_repetition():
+    magazine = ['two', 'times', 'three', 'is', 'not', 'four']
+    note = ['two', 'times', 'two', 'is', 'four']
+    expected = 'No'
+    actual = check_magazine(magazine, note)
+    assert expected == actual
+
+def test_missing_word():
+    magazine = ['ive', 'got', 'a', 'lovely', 'bunch', 'of', 'coconuts']
+    note = ['ive', 'got', 'some', 'coconuts']
+    expected = 'No'
+    actual = check_magazine(magazine, note)
+    assert expected == actual
