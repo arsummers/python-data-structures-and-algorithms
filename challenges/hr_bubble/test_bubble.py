b14ac99ab324311f66c831a26d0fbcd929e0c78e
arsummers <aliyasummers1@gmail.com>
2020-06-03 08:58:55 -0700

    starting setup

---
+++ challenges/hr_bubble/test_bubble.py
@@ -0,0 +1,14 @@
+import pytest
+
+prob_dom = """
+
+Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+
+Array is sorted in numSwaps swaps., where numSwaps is the number of swaps that took place.
+First Element: firstElement, where firstElement is the first element in the sorted array.
+Last Element: lastElement, where lastElement is the last element in the sorted array.
+
+
+Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution
+
+"""
