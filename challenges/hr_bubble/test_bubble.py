3a63414a07ac2b9a88b739e24ef3c243c728f68b
arsummers <aliyasummers1@gmail.com>
2020-06-09 15:19:53 -0700

    bubble sort working

--- challenges/hr_bubble/test_bubble.py
+++ challenges/hr_bubble/test_bubble.py
@@ -1,5 +1,5 @@
 import pytest
-
+from bubble import count_swaps
 prob_dom = """
 
 Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
@@ -12,3 +12,9 @@ Last Element: lastElement, where lastElement is the last element in the sorted a
 Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution
 
 """
+
+def test_bubble_sort():
+    arr = [5, 7, 3, 4, 6, 2, 8, 1, 9]
+    expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    actual = count_swaps(arr)
+    assert expected == actual
