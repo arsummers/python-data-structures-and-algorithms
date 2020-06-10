c5bab16c14905a022fcbdece3741fc925563d1ac
arsummers <aliyasummers1@gmail.com>
2020-06-10 12:13:11 -0700

    tests

---
+++ challenges/max_toys/max_toys.py
@@ -0,0 +1,10 @@
+"""
+Given a list of prices and an amount to spend, what is the maximum number of toys Mark can buy? For example, if prices = [1, 2, 3, 4]  and Mark has k = 7 to spend, he can buy items [1, 2, 3]  for 6, or  [3, 4] for 7 units of currency. He would choose the first group of  items
+"""
+
+
+def max_toys(prices, k):
+    """
+    prices is an array representing toy prices. k is an integer representing the amount Mark can spend
+    """
+    pass
