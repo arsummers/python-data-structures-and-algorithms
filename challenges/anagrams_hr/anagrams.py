9b2f161c4b7e5b30832147c61868757b11da7aa0
arsummers <aliyasummers1@gmail.com>
2020-06-18 16:35:06 -0700

    passing everything but important edgecase

--- challenges/anagrams_hr/anagrams.py
+++ challenges/anagrams_hr/anagrams.py
@@ -6,12 +6,53 @@ a = 'cde'
 b = 'dcf'
 output = 2, since c and f should be deleted
 
+each time there is a discrepency between either the character itself, or the the number of times it appears, increment counter by the difference.
+
+after a and b are sorted and Countered: # thinking these may not be necessary, may only need sorted
+
+loop through a:
+    if i not in b:
+        remove i
+        counter++
+
+loop through b:
+    if i not in a:
+        remove i
+        counter ++
+
+counter += abs(length(a)-length(b))
+
 """
 
 from collections import Counter
 
+
+
 def make_anagram(a, b):
-
-
+    changes = 0
     
-    return
+    a = list(a)
+    b = list(b)
+
+    for i in a:
+        if i not in b:
+            changes += 1
+            # a.remove(i)
+        if i in b:
+            continue
+
+    for i in b:
+        if i not in a:
+            changes += 1
+            # b.remove(i)
+        if i in a:
+            continue
+        
+    changes += abs(len(a) - len(b))
+
+    print('a:', a)
+    print('b:', b)
+    print(changes)
+    return changes
+
+make_anagram('cde', 'abc')
