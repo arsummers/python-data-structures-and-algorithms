d0754c0077aca19c2ee258aeb9bdefeae4cf9585
arsummers <aliyasummers1@gmail.com>
2020-06-24 17:37:07 -0700

    problem domain

---
+++ challenges/alternating_chars/alternating_chars.py
@@ -0,0 +1,7 @@
+"""
+You are given a string containing characters A and B only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+
+Your task is to find the minimum number of required deletions.
+
+For example, given the string s = AABAAB , remove an A at positions 0 and 3 to make s = ABAB in 2 deletions.
+"""
