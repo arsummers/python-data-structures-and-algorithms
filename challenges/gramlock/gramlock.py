72d15d0f8c7fbcbe6fc5699cc36333cacd3f7722
arsummers <aliyasummers1@gmail.com>
2020-07-09 17:59:32 -0700

    not there yet, but feel like I'm closer

--- challenges/gramlock/gramlock.py
+++ challenges/gramlock/gramlock.py
@@ -3,7 +3,37 @@ Two strings are anagrams of each other if the letters of one string can be rearr
 
 Complete the function sherlockAndAnagrams in the editor below. It must return an integer that represents the number of anagrammatic pairs of substrings in s.
 
+notes:
+look for repeating characters
+a single character without a match doesn't count as an anagram
+pair anagrams together
+ - 'cdcd' contains anagrams: [c, c], [d, d], [cd, dc], [cd, cd], [dc, cd]
+ - [cdc] doesn't count as an anagram because it doesn't have a pair
+there can be overlap in which substrings count as pairs:
+    - 'bbbb' contains pairs [bbb, bbb] because that shows up twice as an anagram, even though they overlap at [[1], [2]]
+
+
+basic level:
+break string into all substrings
+    - probably put this into an array to start
+run Counter in each substring
+    - add those to a "parent" Counter dict --> this will weed out non-anagrams, since anything that only shows up once won't get added to the final count
+
+for every item in the counter dict, return the number // 2 --> that will only reveal the things with pairs, no floats.
+
+get all substrings:
+    - ref: https://www.geeksforgeeks.org/python-get-all-substrings-of-given-string/
+            - used itertools version to avoid nesting for loops
+
 """
+from collections import Counter
+from itertools import combinations
 
 def gramlock(s):
-    return
+    substrings = [s[x:y] for x, y in combinations(range(len(s)+1), r=2)]
+    child = Counter(substrings)
+    anagram_count = 0
+    for i in child:
+        if child[i] % 2 == 0:
+            anagram_count += child[i]
+    return anagram_count
