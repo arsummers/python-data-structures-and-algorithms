f0e858893ffad0d532dd871dffb2d03ebd5e893a
arsummers <aliyasummers1@gmail.com>
2020-07-22 14:01:21 -0700

    iterative solution working, recursive won't work with inputs

--- challenges/common_child/test_common_child.py
+++ challenges/common_child/test_common_child.py
@@ -1,14 +1,14 @@
 import pytest
-from common_child import common_child
+from common_child import commonChild
 
 def test_exists():
-    assert common_child
+    assert commonChild
 
 def test_harry():
     s1 = 'HARRY'
     s2 = 'SALLY'
     expected = 2
-    actual = common_child(s1, s2)
+    actual = commonChild(s1, s2)
     assert expected == actual
     # AY
 
@@ -16,14 +16,14 @@ def test_none():
     s1 = 'AA'
     s2 = 'BB'
     expected = 0
-    actual = common_child(s1, s2)
+    actual = commonChild(s1, s2)
     assert expected == actual
 
 def test_shinchan():
     s1 = 'SHINCHAN'
     s2 = 'NOHARAAA'
     expected = 3
-    actual = common_child(s1, s2)
+    actual = commonChild(s1, s2)
     assert expected == actual
     # NHA
 
@@ -31,6 +31,6 @@ def test_abcdf():
     s1 = 'ABCDEF'
     s2 = 'FBDAMN'
     expected = 2
-    actual = common_child(s1, s2)
+    actual = commonChild(s1, s2)
     assert expected == actual
     # BD
