4a2beccdc77b9fbb358af1dfed2187e9652492e0
arsummers <aliyasummers1@gmail.com>
2020-07-23 13:53:44 -0700

    made it oneliner

--- challenges/abs_minimum/abs_minimum.py
+++ challenges/abs_minimum/abs_minimum.py
@@ -15,13 +15,17 @@ update minimum if the new value is smaller than the current one
 from itertools import combinations
 
 def abs_minimum(arr):
-    pairs = list(combinations(arr, 2))
-    starter = pairs[0]
-    minimum = abs(starter[0] - starter[1])
+    # pairs = list(combinations(arr, 2))
+    # starter = pairs[0]
+    # minimum = abs(starter[0] - starter[1])
 
-    for i in pairs:
-        current = abs(i[0] - i[1])
-        if current < minimum:
-            minimum = current
+    # for i in pairs:
+    #     current = abs(i[0] - i[1])
+    #     if current < minimum:
+    #         minimum = current
 
-    return minimum
+    # return minimum
+    arr.sort()
+
+    return min([abs(arr[i + 1] - arr[i]) for i in range(len(arr)-1)])
+
