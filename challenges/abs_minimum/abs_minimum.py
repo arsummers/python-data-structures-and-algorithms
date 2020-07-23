58ef41ac55fac8aa8033375752ec950355708dda
arsummers <aliyasummers1@gmail.com>
2020-07-23 13:42:26 -0700

    passes base cases

---
+++ challenges/abs_minimum/abs_minimum.py
@@ -0,0 +1,27 @@
+"""
+Given an array of integers, find and print the minimum absolute difference between any two elements in the array. 
+
+in: arr = [-2, 2, 4]
+out: 2
+
+algorithm:
+get all possible pairs using itertools
+from each pair 
+have base minimum set to the value of the first pair's difference
+iterate over each pair
+update minimum if the new value is smaller than the current one
+
+"""
+from itertools import combinations
+
+def abs_minimum(arr):
+    pairs = list(combinations(arr, 2))
+    starter = pairs[0]
+    minimum = abs(starter[0] - starter[1])
+
+    for i in pairs:
+        current = abs(i[0] - i[1])
+        if current < minimum:
+            minimum = current
+
+    return minimum
